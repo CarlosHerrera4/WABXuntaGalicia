383451a1d3a495d113cdb0987c4a22d3bbcf92e4
MundoGister <mundogister@gmail.com>
2016-07-05 10:37:13 +0200

    Add demo

--- Visor/env.js
+++ Visor/env.js
@@ -89,7 +89,7 @@ var
 
   ////////uncomment the following line when downloading the app
 
-  apiUrl = '//js.arcgis.com/3.16';
+  //apiUrl = '//js.arcgis.com/3.16';
 
   //////////////////////////////////////////////////////////////
   allCookies = getAllCookies();
